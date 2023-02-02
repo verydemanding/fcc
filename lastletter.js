0ca7ef3510130e529687daed72775b06a1e065d2
verydemanding <58390763+verydemanding@users.noreply.github.com>
2023-02-02 12:14:26 +0200

    Update lastletter.js

--- lastletter.js
+++ lastletter.js
@@ -2,5 +2,6 @@
 const lastName = "Lovelace";
 
 // Only change code below this line
-const lastLetterOfLastName = lastName[lastName.length - 1]; // lastName.length is 8 so -1 is 7 and seventh letter is e if we count from 0.
+const lastLetterOfLastName = lastName[lastName.length - 1]; 
+// lastName.length = 8 so -1 is 7 and seventh letter is e because we count from 0.
 
