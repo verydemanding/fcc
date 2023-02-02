a065a3294853d4c7fe7f3c13bf38a21cc27fb71d
verydemanding <58390763+verydemanding@users.noreply.github.com>
2023-02-02 13:39:55 +0200

    Create arrayShift.js

---
+++ arrayShift.js
@@ -0,0 +1,13 @@
+const myArray = [["John", 23], ["dog", 3]];
+
+/*Use the .shift() function to remove the first item from myArray and 
+assign the "shifted off" value to a new variable, removedFromMyArray.*/
+
+const removedFromMyArray = myArray.shift();
+
+//example
+
+const ourArray = ["Stimpson", "J", ["cat"]];
+const removedFromOurArray = ourArray.shift();
+
+//removedFromOurArray would have a value of the string Stimpson, and ourArray would have ["J", ["cat"]].
