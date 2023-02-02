f65499f06c5b11c3204a32c03c697fd0a7fc76c5
verydemanding <58390763+verydemanding@users.noreply.github.com>
2023-02-02 12:55:28 +0200

    Create pushInToArray.js

---
+++ pushInToArray.js
@@ -0,0 +1,16 @@
+const myArray = [["John", 23], ["cat", 2]];
+
+// Push ["dog", 3] onto the end of the myArray variable.
+
+myArray.push = (["dog", 3])
+
+// examples
+
+const arr1 = [1, 2, 3];
+arr1.push(4);
+
+const arr2 = ["Stimpson", "J", "cat"];
+arr2.push(["happy", "joy"]);
+
+/* arr1 now has the value [1, 2, 3, 4] and 
+arr2 has the value ["Stimpson", "J", "cat", ["happy", "joy"]]. */
