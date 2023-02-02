fca4946f75f2f62f7ab93399b28f4822527b4995
verydemanding <58390763+verydemanding@users.noreply.github.com>
2023-02-02 12:32:03 +0200

    removing useless comments

--- lastletter.js
+++ lastletter.js
@@ -1,7 +1,5 @@
-// Setup
 const lastName = "Lovelace";
 
-// Only change code below this line
 const lastLetterOfLastName = lastName[lastName.length - 1]; 
 // lastName.length = 8 so -1 is 7 and seventh letter is e because we count from 0.
 
